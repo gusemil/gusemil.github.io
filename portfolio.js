f1376cc4535e393ff79764d30020a4d0b1026b86
gusemil <gusemil@hotmail.com>
2024-09-10 21:55:45 +0300

    FIX: Hide navbar links on mobile, collapse nav dropdown after click

--- portfolio.js
+++ portfolio.js
@@ -1,7 +1,7 @@
-//PopOver
-const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
-
 //Tooltip
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+$('.nav-link').click(function() {
+    $('.navbar-collapse').collapse('hide');
+});
