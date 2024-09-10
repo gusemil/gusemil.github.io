3e7ee842f92aa3888806b5e97daa919b784e2d33
gusemil <gusemil@hotmail.com>
2024-09-10 22:15:42 +0300

    FIX: Hide footer on nav dropdown toggle and show on click

--- portfolio.js
+++ portfolio.js
@@ -2,6 +2,16 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
+$('.navbar-toggler').click(function() {
+    if($('.my-footer').is(":hidden")){
+        $('.my-footer').show();
+    }
+    else{
+        $('.my-footer').hide();
+    }
+});
+
 $('.nav-link').click(function() {
     $('.navbar-collapse').collapse('hide');
+    $('.my-footer').show();
 });
