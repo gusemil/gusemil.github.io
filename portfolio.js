98c0fa0a4c5e5d9ce7a0c008945d29416a5cca2f
gusemil <gusemil@hotmail.com>
2024-09-16 11:48:12 +0300

    FIX: Tooltip on mouse leave hide works properly

--- portfolio.js
+++ portfolio.js
@@ -15,3 +15,24 @@ $('.nav-link').click(function() {
     $('.navbar-collapse').collapse('hide');
     $('.my-footer').show();
 });
+
+$('.my-tooltip').on("mouseleave",function() {
+    for(i = 0; i < tooltipList.length; i++){
+        tooltipList[i].hide();
+    }
+});
+
+/*
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').click(function () {
+       $('[data-toggle="tooltip"]').tooltip("hide");
+
+    });
+
+    for(i = 0; i < tooltipList.length; i++){
+        tooltipList.addEventListener('focus', (e) => {
+            e.target.tooltipList[i].disable();
+        });
+    }
+});
+*/
