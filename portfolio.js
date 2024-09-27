7e7cb8238d8ebcd5c3d95f2bea61458978722c24
gusemil <gusemil@hotmail.com>
2024-09-27 22:42:47 +0300

    FIX: Disable tooltips on mobile

--- portfolio.js
+++ portfolio.js
@@ -1,7 +1,3 @@
-//Tooltip
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
-
 $('.navbar-toggler').click(function() {
     if($('.my-footer').is(":hidden")){
         $('.my-footer').show();
@@ -15,6 +11,29 @@ $('.nav-link').click(function() {
     $('.navbar-collapse').collapse('hide');
     $('.my-footer').show();
 });
+
+//Tooltips
+let tooltipTriggerList;
+let tooltipList;
+
+function toggleTooltips(x) {
+    if (x.matches) {
+        tooltipList.forEach(function (tooltip, index) {
+            tooltip.dispose();
+          });
+    } else {
+      tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+      tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+    }
+  }
+
+  let x = window.matchMedia("(max-width: 992px)")
+
+  toggleTooltips(x);
+  
+  x.addEventListener("change", function() {
+    toggleTooltips(x);
+  }); 
 
 $('.my-tooltip').on("mouseleave",function() {
     for(i = 0; i < tooltipList.length; i++){
